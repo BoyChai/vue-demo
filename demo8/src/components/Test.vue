103a8c4eabacf3dab086a822357ef3a0fa0ef070
BoyChai <1972567225@qq.com>
2023-02-19 23:32:05 +0800

    demo8 - TestComponents

---
+++ demo8/src/components/Test.vue
@@ -0,0 +1,33 @@
+<!--创建组件-->
+<template>
+  <div>
+    <h1 v-if="msg==null" class="msg">{{ message }}</h1>
+    <h1 v-else class="msg">{{ msg }}</h1>
+    <button @click="btn()">点我向父组件传递数据</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TestComponents",
+  data() {
+    return {
+      message: "Hello Vue!",
+    };
+  },
+  props: {  // 父传子参数
+    msg: Array,
+  },
+  methods: {
+    btn() {
+      this.$emit("giao",this.message)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.msg {
+  color: red;
+}
+</style>
---
+++ demo8/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+<!--    使用组件-->
+    <TestComponents msg="123" @giao="giao"></TestComponents>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+// 导入文件
+// @符号代表src目录
+import TestComponents from "@/components/Test.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+    // 导入组件
+    TestComponents,
+  },
+  methods: {
+    giao(data) {
+      console.log(data)
+    }
+  }
+};
+</script>
