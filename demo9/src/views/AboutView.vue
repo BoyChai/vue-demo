fd0e1caab7b02bc803a3a61173fae61ea2a80499
BoyChai <1972567225@qq.com>
2023-02-20 22:32:16 +0800

    demo9 - axios1

---
+++ demo9/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="getData">调用axios</button>
+    <h3 v-show="user">用户信息</h3>
+    <ul v-for="item in user.data" :key="item.id" >
+      <li>{{item.name}}</li>
+      <li>{{item.email}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: "",
+    }
+  },
+  methods: {
+    getData() {
+      // this.$axios.get('http://api.boychai.xyz/test/v1/user').then( (resp)=> {
+      this.$axios.get('/test/v1/user').then( resp=> {
+        this.user = resp.data
+      })
+      console.log(this.user)
+    }
+  }
+}
+
+</script>
